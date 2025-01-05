2192da88071873c938101923301d9d2fd6804a4e
Renã Souza <rena.souza@ubots.com.br>
2025-01-04 22:23:21 -0300

    Major refactor

--- src/Popup.svelte
+++ src/Popup.svelte
@@ -1,185 +1,208 @@
 <script>
-  import DeclassifyService from './services/declassify.service.js'; // Statically import the service
-  let url = '';
-  let isModalVisible = false;
-  let results = [];
-  let isLoading = false;
+    import DeclassifyService from './services/declassify.service.js'; // Statically import the service
+    import {onMount} from "svelte";
 
-  // Function to get the URL if it's a PDF
-  function getUrl() {
-    const currentUrl = window.location.href;
+    let imageUrl;
+    let url
+    let isModalVisible = false;
+    let results = [];
+    let isLoading = false;
 
-    if (currentUrl.endsWith(".pdf")) {
-      return currentUrl;
+    function getUrl() {
+        const currentUrl = window.location.href;
+
+        if (currentUrl.endsWith(".pdf")) {
+            return currentUrl;
+        }
+
+        const plugin = document.querySelector('embed[type="application/pdf"]');
+        if (plugin && plugin.getAttribute("type") == "application/pdf") {
+            return currentUrl;
+        }
+        return null;
     }
 
-    const plugin = document.querySelector('embed[type="application/pdf"]');
-    if (plugin && plugin.getAttribute("type") == "application/pdf") {
-      return currentUrl;
+    function showModal() {
+        isModalVisible = true;
     }
-    return null;
-  }
 
-  // Function to show the modal
-  function showModal() {
-    isModalVisible = true;
-  }
+    function hideModal() {
+        isModalVisible = false;
+    }
 
-  // Function to hide the modal
-  function hideModal() {
-    isModalVisible = false;
-    results = []; // Clear results when modal is closed
-  }
+    function addResult(name, result) {
+        results = [...results, {name, result}];
+    }
 
-  // Function to add classification result
-  function addResult(name, result) {
-    results = [...results, { name, result }];
-  }
+    async function classifyDocument() {
+        const declassify = new DeclassifyService();
+        isLoading = true;
+        const {results: newResults} = await declassify.classify(url);
+        isLoading = false;
 
-  // On component mount, get the URL
-  // Fetch classification results on mount
-  async function classifyDocument() {
-    const declassify = new DeclassifyService();
-    isLoading = true;
-    const { results: newResults } = await declassify.classify(url);
-    isLoading = false;
+        // Add classification results
+        addResult("Nature", `${newResults.nature}`);
+        addResult("Method", `${newResults.method}`);
+        addResult("Validation Strategy", `${newResults.validationStrategy}`);
+        addResult("Data Nature", `${newResults.dataNature}`);
+        addResult("Environment", `${newResults.environment}`);
+        addResult("Methodological", `${newResults.methodological}`);
+        addResult("Proof", `${newResults.proof}`);
+        addResult("Purpose", `${newResults.purpose}`);
+        addResult("Secondary Proof", `${newResults.secondaryProof}`);
+        addResult("Validation Result", `${newResults.validationResult}`);
+    }
 
-    // Add classification results
-    addResult("Nature", `${newResults.nature}`);
-    addResult("Method", `${newResults.method}`);
-    addResult("Validation Strategy", `${newResults.validation_strategy}`);
-    addResult("Data Nature", `${newResults.data_nature}`);
-    addResult("Environment", `${newResults.environment}`);
-    addResult("Methodological", `${newResults.methodological}`);
-    addResult("Proof", `${newResults.proof}`);
-    addResult("Purpose", `${newResults.purpose}`);
-    addResult("Secondary Proof", `${newResults.secondary_proof}`);
-    addResult("Validation Result", `${newResults.validation_result}`);
-  }
+    url = getUrl();
 
-  // Initialize URL on component mount
-  url = getUrl();
-
-  // Call classifyDocument when component is ready
-  classifyDocument();
+    classifyDocument();
+    onMount(() => {
+        imageUrl = chrome.runtime.getURL("images/owl-big.png");
+    });
 </script>
 
 {#if url}
-  <!-- Main button to open the modal -->
-  <div on:click={showModal} id="pdf-classify">
-    <p >Declassify</p>
-  </div>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet">
+    <div id="wrapper">
+        <!-- Modal button -->
+        {#if !isModalVisible}
+            <div on:click={showModal} id="modal-button">
+                {#if imageUrl}
+                    <img src={imageUrl} alt="Owl Image" id="modal-button-img"/>
+                {/if}
+            </div>
+        {/if}
 
-  <!-- Modal content -->
-  {#if isModalVisible}
-    <div id="pdf-classify-modal">
-      <div id="pdf-classify-modal-close-container">
-        <div id="pdf-classify-modal-close-button" on:click={hideModal}>✖</div>
-      </div>
-      <h2>Classification</h2>
-      <div id="pdf-classify-results">
-        {#each results as { name, result }}
-          <p><strong>{name}</strong>: {result}</p>
-        {/each}
-      </div>
-      {#if isLoading}
-        <div id="pdf-classify-loader">
-          <span></span>
-        </div>
-      {/if}
+        <!-- Modal content -->
+        {#if isModalVisible}
+            <div id="modal">
+                <div id="modal-close-container">
+                    <div id="modal-close-button" on:click={hideModal}>✖</div>
+                </div>
+                <div id="title"><span>De</span><span>classify</span></div>
+                <div id="document-title">Expressing Computer Science Concepts Through Kodu Game Lab</div>
+                <div id="pdf-classify-results">
+                    {#each results as {name, result}}
+                        <p><strong>{name}</strong>: {result}</p>
+                    {/each}
+                </div>
+                {#if isLoading}
+                    <div id="pdf-classify-loader">
+                        <span></span>
+                    </div>
+                {/if}
+            </div>
+        {/if}
     </div>
-  {/if}
 {/if}
 
 <style>
-  #pdf-classify {
-    position: fixed;
-    background-color: rgb(50, 54, 57);
-    width: 86px;
-    height: 86px;
-    border-radius: 100%;
-    right: 32px;
-    bottom: 32px;
-    align-items: center;
-    justify-content: center;
-    display: flex;
-    color: white;
-    font-size: .87rem;
-    font-weight: 600;
-    font-family: system-ui, sans-serif;
-    display: flex;
-    cursor: pointer;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  }
-
-  #pdf-classify:hover {
-    background-color: rgb(33, 36, 38);
-    transition: background-color .3s;
-  }
-
-  #pdf-classify > p {
-    margin: 0;
-    padding: 0;
-  }
-
-  #pdf-classify-modal {
-    font-family: sans-serif;
-    position: fixed;
-    margin: 0 auto;
-    background-color: white;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    width: 300px;
-    height: 400px;
-    border-radius: 8px;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 8px;
-  }
-
-  #pdf-classify-modal-close-container {
-    display: flex;
-    justify-content: flex-end;
-  }
-
-  #pdf-classify-modal-close-button {
-    cursor: pointer;
-  }
-
-  #pdf-classify-modal h2 {
-    text-align: center;
-    margin: 0;
-    margin-bottom: 8px;
-  }
-
-  #pdf-classify-results {
-    padding: 0 16px;
-  }
-
-  #pdf-classify-loader > span {
-    width: 48px;
-    height: 48px;
-    border: 5px solid #000;
-    border-bottom-color: transparent;
-    border-radius: 50%;
-    display: inline-block;
-    box-sizing: border-box;
-    animation: rotation 1s linear infinite;
-  }
-
-  @keyframes rotation {
-    0% {
-      transform: rotate(0deg);
+    #wrapper {
+        position: fixed;
+        right: 32px;
+        bottom: 0;
+        display: flex;
+        align-items: flex-end;
+        flex-direction: column;
     }
-    100% {
-      transform: rotate(360deg);
+
+    #modal-button-img {
+        height: 100%;
     }
-  }
 
-  #pdf-classify-loader {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+    #modal-button {
+        width: 74px;
+        height: 74px;
+        border-radius: 100%;
+        cursor: pointer;
+        box-shadow: rgba(0, 0, 0, 0.25) 5px 10px 15px 5px;
+        overflow: hidden;
+        margin-bottom: 20px;
+    }
 
+    #modal-button:hover {
+        background-color: rgb(33, 36, 38);
+        transition: background-color .3s;
+    }
+
+    #modal-button > p {
+        margin: 0;
+        padding: 0;
+    }
+
+    #modal {
+        font-family: sans-serif;
+        background-color: white;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+        width: 230px;
+        border-radius: 8px 8px 0 0;
+        padding: 20px;
+    }
+
+    #modal-close-container {
+        display: flex;
+        justify-content: flex-end;
+        height: 0px;
+    }
+
+    #modal-close-button {
+        cursor: pointer;
+    }
+
+    #modal #title {
+        text-align: center;
+        margin: 0;
+        margin-bottom: 8px;
+        font-family: Inter;
+        font-size: 28px;
+    }
+
+    #modal #title > span:nth-child(1) {
+        font-weight: 200;
+        text-decoration: underline;
+    }
+
+    #modal #title > span:nth-child(2) {
+        font-weight: bold;
+    }
+
+    #pdf-classify-results {
+        border-bottom: 1px solid black
+    }
+
+    #pdf-classify-results p {
+        margin: 10px 0;
+    }
+
+    #document-title {
+        color: #1D7392;
+        text-decoration: underline;
+    }
+
+    #pdf-classify-loader > span {
+        width: 48px;
+        height: 48px;
+        border: 5px solid #000;
+        border-bottom-color: transparent;
+        border-radius: 50%;
+        display: inline-block;
+        box-sizing: border-box;
+        animation: rotation 1s linear infinite;
+    }
+
+    @keyframes rotation {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    #pdf-classify-loader {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
